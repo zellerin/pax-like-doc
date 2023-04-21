05d36463e85316b9fc95fb7f488f61ed91d8b9b1
Tomáš Zellerin <tomas@zellerin.cz>
2023-04-21 12:39:55 +0200

    (feat) Allow using groups for emacs fns and variables

--- emacs/doc-dblocks.el
+++ emacs/doc-dblocks.el
@@ -1,14 +1,57 @@
-(defun tz-documentation (fn-names)
-  (dolist (fn-name fn-names)
-    (insert "- [[help:" (symbol-name fn-name) "]["
-            (symbol-name fn-name) "]] :: "
-            (if (commandp fn-name) "=cmd=" "=fn="))
-    (dolist (line (split-string  (documentation fn-name t) "\n"))
-      (insert "  " line "\n"))))
+(define-short-documentation-group tz-doc
+  (org-dblock-write:elisp-fns-doc)
+  (sly-edit-section))
+
+(defun emacs-docstring-to-org ()
+  "Convert Emacs doc string to org format.
+
+Replace backticked references (`princ`) with help links."
+  (goto-char (point-min))
+  (while (re-search-forward "‘\\([a-z-]+\\)’" nil t)
+    (replace-match "[[help:\\1][\\1]]")))
+
+(defun doc-dblock-expand-groups (group)
+  "Extract names from a shortdoc groups."
+  (when (stringp group)
+    (setq group (intern group)))
+  (let ((items (assq group shortdoc--groups)))
+    (unless items (user-error "No such documentation group %s" group))
+    (pop items)
+    (remove nil (mapcar (lambda (item) (unless (stringp item) (car item))) items))))
+
+(defun tz-documentation (fn-names short-doc-group custom-group)
+  (dolist (fn-name (append (doc-dblock-expand-groups short-doc-group)
+                           fn-names))
+    (when (functionp fn-name)
+      (insert "- [[help:" (symbol-name fn-name) "]["
+              (symbol-name fn-name) "]] :: "
+              (if (commandp fn-name) "=cmd=" "=fn="))
+      (dolist (line (split-string  (documentation fn-name t) "\n"))
+        (insert "  " line "\n"))))
+
+  (dolist (var (custom-group-members custom-group nil))
+    (let ((doc (documentation-property (car var) 'variable-documentation)))
+      (insert "- [[help:" (symbol-name (car var)) "]["
+              (symbol-name (car var)) "]] :: =" (symbol-name (second var)) "=")
+      (insert
+       (with-temp-buffer
+         (dolist (line (split-string (or doc "Undocumented") "\n"))
+           (insert "  " line "\n"))
+         (emacs-docstring-to-org)
+         (buffer-substring-no-properties (point-min) (point-max)))))))
 
 ;;;###autoload
 (defun org-dblock-write:elisp-fns-doc (&optional params)
-  (tz-documentation (plist-get params :fns)))
+  "Write documentation from the elisp-fns-doc.
+
+Used headers:
+- :short-doc-group to describe a group defined by DEFINE-SHORT-DOCUMENTATION-GROUP
+- :symbols for a list of symbols to describe
+- :custom-group for a customization group
+- :group when :custom-group and short-doc-group are same"
+  (tz-documentation (plist-get params :symbols)
+                    (or (plist-get params :short-doc-group) (plist-get params :group))
+                    (or (plist-get params :custom-group) (plist-get params :group))))
 
 ;;;###autoload
 (define-skeleton org-dblock-create-elisp-block
